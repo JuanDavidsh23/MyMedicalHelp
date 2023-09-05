f44240386453eb6c46e389ccb4153d84aa4d073e
jordy425 <jordyjesusd@gmail.com>
2023-09-04 23:22:37 -0500

    Mostrar icon de la pagina

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-<link rel="icon" type="image/png" href="{{ asset('dist/img/log.jpg') }}">
+<link rel="icon" type="image/png" href="{{ asset('dist/img/log.jpg.png') }}">
 
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
