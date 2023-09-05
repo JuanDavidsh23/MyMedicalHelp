8e0a05a4c4d0977ee52900c3530c26c94cb8c895
jordy425 <jordyjesusd@gmail.com>
2023-09-05 08:38:45 -0500

    nuevo color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,7 +68,7 @@
   <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <aside class="main-sidebar elevation-4" style="background-color: #fff;">
     <!-- Brand Logo -->
 
     <!-- Sidebar -->
